f9105add2d1a7d1a304bc39ce848e9a6830bba0e
kurone-kito <krone@kit.black>
2020-07-04 23:53:03 +0900

    feat: added the template

---
+++ src/components/templates/Karte.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import NoQuestiohns from '~/components/atoms/NoQuestiohns';
+import Heading from '~/components/organisms/Heading';
+import Loading from '~/components/organisms/Loading';
+import Questions from '~/components/organisms/Questions';
+import Result from '~/components/organisms/Result';
+import ToggleResultButton from '~/components/organisms/ToggleResultButton';
+import { UsecaseAndAnswer } from '~/hooks/useUsecaseAndAnswer';
+
+export interface DOMProps {
+  children?: never;
+  fail?: boolean;
+  loaded?: boolean;
+  result?: boolean;
+}
+
+export const DOM: React.FC<DOMProps> = ({ fail, loaded, result }) => (
+  <article>
+    {loaded && (
+      <>
+        <Heading />
+        <Questions />
+        {result ? (
+          <Result />
+        ) : (
+          <ToggleResultButton condition={(result) => result}>
+            結果を見る！
+          </ToggleResultButton>
+        )}
+      </>
+    )}
+    {fail && <NoQuestiohns />}
+    <Loading />
+  </article>
+);
+DOM.displayName = 'KarteDOM';
+
+const Container: React.FC = () => {
+  const { fail, loaded, result } = UsecaseAndAnswer.useContainer();
+  return <DOM fail={fail} loaded={loaded} result={result} />;
+};
+Container.displayName = 'Karte';
+
+export default Container;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import Karte from '~/components/templates/Karte';
+import { UsecaseAndAnswer } from '~/hooks/useUsecaseAndAnswer';
 
-const Component: React.FC = () => <>Hello, world!</>;
+const Component: React.FC = () => (
+  <UsecaseAndAnswer.Provider>
+    <Karte />
+  </UsecaseAndAnswer.Provider>
+);
 Component.displayName = 'App';
 
 export default Component;
